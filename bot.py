cc13c69d8b54dcda7185072cd84ddc74db949fc1
NSD00 <gg.nsd0@gmail.com>
2026-01-07 00:57:33 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -310,7 +310,7 @@ def main():
     ensure_dirs()
     cleanup_old_applications()
 
-    app = ApplicationBuilder().token(BOT_TOKEN).build()
+   # app = ApplicationBuilder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("status", status))
@@ -325,4 +325,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
