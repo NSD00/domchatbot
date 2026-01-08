251c469d517b1f917d4e3f0ecebd04a51a8dfdce
NSD00 <gg.nsd0@gmail.com>
2026-01-08 22:15:25 +0800

    Delete web.py

--- web.py
+++
@@ -1,7 +0,0 @@
-﻿from bot import app
-import os
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
-    print(f"Starting web server on port {port}")
-    app.run(host='0.0.0.0', port=port, debug=False, threaded=True)
